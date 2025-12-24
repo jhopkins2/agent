7147f6a490a9becda35f7e786656a3efdbce099f
jhopkins2 <jhopkins2@islander.tamucc.edu>
2025-12-24 11:21:31 -0600

    Added arg parser support and conversion history support

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from google import genai
 from dotenv import load_dotenv
 
@@ -7,9 +8,20 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    parser = argparse.ArgumentParser(description="Coding Support")
+    parser.add_argument("user_prompt", type=str, help="Provide a question or prompt to ask the agent for help")
+    args = parser.parse_args()
 
-    print(response.text)
+    messages = [genai.types.Content(role="user", parts=[genai.types.Part(text=args.user_prompt)])]
+
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+
+    if response.usage_metadata != None:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(response.text)
+    else:
+        raise RuntimeError("No response from Gemini model")
 
 
 if __name__ == "__main__":
