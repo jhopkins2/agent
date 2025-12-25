716f8352f8a573f3df0a99985f6c541a49c6cde0
jhopkins2 <jhopkins2@islander.tamucc.edu>
2025-12-24 21:56:52 -0600

    added the function description functionality for the agent to know what function to call

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 import argparse
-from google import genai
+from google.genai import Client, types
 from dotenv import load_dotenv
 from prompts.system_prompt import system_prompt
+from call_function import available_functions
 
 def main():
     load_dotenv()
@@ -15,13 +16,16 @@ def main():
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output of the coding agent")
     args = parser.parse_args()
 
-    client = genai.Client(api_key=api_key)
-    messages = [genai.types.Content(role="user", parts=[genai.types.Part(text=args.user_prompt)])]
+    client = Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
         
     response = client.models.generate_content(
             model="gemini-2.5-flash", 
             contents=messages,
-            config=genai.types.GenerateContentConfig(system_instruction=system_prompt))
+            config=types.GenerateContentConfig(
+                tools = [available_functions],
+                system_instruction=system_prompt)
+            )
 
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
@@ -31,7 +35,12 @@ def main():
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    print(f"Response: {response.text}")
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        if response.text:
+            print(f"Response: {response.text}")
 
 if __name__ == "__main__":
     main()
