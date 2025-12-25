09e3798edeae50ca1fc60ff9770f434a201076e5
jhopkins2 <jhopkins2@islander.tamucc.edu>
2025-12-24 20:24:37 -0600

    Added system level prompt support. Refactored main.py a bit to flow better.

--- main.py
+++ main.py
@@ -2,31 +2,36 @@ import os
 import argparse
 from google import genai
 from dotenv import load_dotenv
+from prompts.system_prompt import system_prompt
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
-    client = genai.Client(api_key=api_key)
     parser = argparse.ArgumentParser(description="Coding Support")
     parser.add_argument("user_prompt", type=str, help="Provide a question or prompt to ask the agent for help")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output of the coding agent")
     args = parser.parse_args()
 
+    client = genai.Client(api_key=api_key)
     messages = [genai.types.Content(role="user", parts=[genai.types.Part(text=args.user_prompt)])]
+        
+    response = client.models.generate_content(
+            model="gemini-2.5-flash", 
+            contents=messages,
+            config=genai.types.GenerateContentConfig(system_instruction=system_prompt))
 
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
 
-    if response.usage_metadata and args.verbose:
+    if args.verbose:
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        print(response.text)
-    elif response.usage_metadata and not args.verbose:
-        print(response.text)
-    else:
-        raise RuntimeError("No response from Gemini model")
 
+    print(f"Response: {response.text}")
 
 if __name__ == "__main__":
     main()
